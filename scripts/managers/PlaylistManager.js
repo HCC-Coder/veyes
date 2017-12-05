1b310ddde5f3ad9b022c9d4dbbec77a2aeeed8c5
Elliot Yap <yilliot@gmail.com>
2017-12-05 11:07:28 +0800

    rewamp playlist manager

--- scripts/managers/PlaylistManager.js
+++ scripts/managers/PlaylistManager.js
@@ -1,37 +1,65 @@
-const jsonfile = require('jsonfile');
 const Playlist = require('./../models/Playlist.js');
+
+const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 class PlaylistManager{
 
   constructor() {
+
+    // default value
     this._playlist_filename = 'default'
     this._items = {};
 
-    if (!fs.existsSync('playlists/' + this._playlist_filename + '.json')) {
-      jsonfile.writeFileSync('playlists/' + this._playlist_filename + '.json', [])
-    }
+    // init
+    this.init_default_playlist_storage()
+    this.init_load_playlists_from_storage()
+    this.init_event()
 
-    this.initPlaylistFromStorage()
-    this.loadCurrentPlaylist()
-    // this._playlist = new Playlist('default');
+    this.load_current_playlist()
   }
 
   get current_playlist() {
     return this._items[this._playlist_filename + '.json']
   }
 
-  initPlaylistFromStorage()
+  init_event()
   {
+    var that = this
+
+    $('#playlist .remove.button').click(function(){
+      that.current_playlist.delete_video($(this).data('id'))
+      that.load_current_playlist()
+      that.save_playlists_to_storage()
+    })
+    $('#playlist .play-item').click(function(){
+      $('#playlist tr').removeClass('positive')
+      $(this).addClass('positive')
+      that.current_playlist.set_to_be_played($(this).data('id'))
+    })
+  }
+
+  init_default_playlist_storage()
+  {
+    if (!fs.existsSync('playlists/' + this._playlist_filename + '.json')) {
+      jsonfile.writeFileSync('playlists/' + this._playlist_filename + '.json', [])
+    }
+  }
+
+  init_load_playlists_from_storage()
+  {
+    $('#dropdown-playlists').html('');
     let files = fs.readdirSync('playlists');
     for(var i in files) {
       if (files[i] != '.gitignore') {
+        let item_html = `<option value="${files[i]}"> ${files[i]} </option>`;
+        $('#dropdown-playlists').append(item_html)
         this._items[files[i]] = new Playlist(jsonfile.readFileSync('playlists/' + files[i]));
       }
     }
   }
 
-  loadCurrentPlaylist() {
+  load_current_playlist() {
     var that = this;
     $('#playlist').html('')
     let items = this.current_playlist.get_array();
@@ -43,25 +71,16 @@ class PlaylistManager{
       </tr>`
       $('#playlist').append(item_html)
     }
-    $('#playlist .remove.button').click(function(){
-      that.current_playlist.delete_video($(this).data('id'))
-      that.loadCurrentPlaylist()
-      that.savePlaylists()
-    })
-    $('#playlist .play-item').click(function(){
-      $('#playlist tr').removeClass('positive')
-      $(this).addClass('positive')
-      that.current_playlist.set_to_be_played($(this).data('id'))
-    })
+    this.init_event()
   }
 
   addToCurrentPlaylist(filename){
     this.current_playlist.add_video(filename)
-    this.savePlaylists()
-    this.loadCurrentPlaylist()
+    this.save_playlists_to_storage()
+    this.load_current_playlist()
   }
 
-  savePlaylists() {
+  save_playlists_to_storage() {
     for(let filename in this._items){
       let item = this._items[filename];
       jsonfile.writeFileSync('playlists/' + filename, item.get_array())
